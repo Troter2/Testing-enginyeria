bcce6ba238769a1a98cf4fb8e6ca87e7ab44a4fd
jus25 <jus25gamer@gmail>
2023-12-28 20:17:14 +0100

    Actualització Nif, password i tests d'aquestos

--- src/data/Nif.java
+++ src/data/Nif.java
@@ -2,17 +2,26 @@ package data;
 
 public class Nif {
     private final String nif;
+
     public Nif(String nif1) throws InvalidNifException {
-        if (nif1 == null) {
+        validateNif(nif1);
+        this.nif = nif1;
+    }
+
+    private void validateNif(String nif) throws InvalidNifException {
+        if (nif == null) {
             throw new InvalidNifException("El NIF no puede ser null");
-        } else if (nif1.length() != 9) {
+        } else if (nif.length() != 9) {
             throw new InvalidNifException("El NIF debe tener exactamente 9 caracteres");
-        } else if (!nif1.substring(0, 8).matches("\\d+") || !Character.isLetter(nif1.charAt(8))) {
+        } else if (!isValidNifFormat(nif)) {
             throw new InvalidNifException("El formato del NIF es incorrecto");
-        } else {
-            this.nif = nif1;
         }
     }
+
+    private boolean isValidNifFormat(String nif) {
+        return nif.substring(0, 8).matches("\\d+") && Character.isLetter(nif.charAt(8));
+    }
+
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
@@ -23,8 +32,9 @@ public class Nif {
         Nif other = (Nif) obj;
         return this.nif.equals(other.nif);
     }
+
     public static class InvalidNifException extends Exception {
-        public InvalidNifException (String msg){
+        public InvalidNifException(String msg) {
             super(msg);
         }
     }
